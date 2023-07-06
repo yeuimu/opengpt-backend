b7253cfdc95dab57ddfdf4e4934d27e5c6469c76
Yoyoki <2197651308@qq.com>
2023-07-06 14:17:17 +0800

    The code block to send the code added `trycatch` statements

--- middleware/code.js
+++ middleware/code.js
@@ -4,6 +4,7 @@ const { auth } = require('../model/mongodb');
 const apiSendCode = express.Router();
 const sendmail = require('../src/sendmail');
 const redisClient = require('../model/redis');
+const colors = require('colors-console');
 
 // The func to generate verify code
 const generateCode = () => String(Math.floor(Math.random() * 1000000)).padEnd(6, '0');
@@ -21,18 +22,24 @@ async (req, res) => {
   // To verify valid of the email
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() })
+    return res.status(422).json({ errors: errors.array() })
   }
 
   // The verify code is sending to user with own QQ Mail and set which in redis
   const email = req.body.email;
   const code = generateCode();
-  if(await sendmail(email, code)) {
-    redisClient.set(email, code);
-    redisClient.expire(email, 60 * 10); // 10 minute
-    res.send({ message: 'Verify code is sended'})
+  try {
+    if(await sendmail(email, code)) {
+      redisClient.set(email, code);
+      redisClient.expire(email, 60 * 10); // 10 minute
+      res.send({ message: 'Verify code is sended'})
+    } else res.status(500).json({message: 'Failed to send code'}); 
+  } catch (error) {
+    if (error) {
+      console.log(colors('red', 'Code:'), `${email} 验证码发送过程中，系统错误\n`, error);
+      res.status(500).json({message: 'An error has occurred in code api'})
+    }
   }
-  else res.status(500).json({message: 'Failed to send code'});
 })
 
 module.exports = apiSendCode;
