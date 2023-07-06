27a5796dac97e16a50322b403f4f192690f6e68b
Yoyoki <2197651308@qq.com>
2023-07-06 11:44:39 +0800

    Delete the test code

--- src/token.js
+++
@@ -1,7 +0,0 @@
-const verifyToken = (reqClient, resClient, next) => {
-    console.log('Verify token...')
-    if (!true) resClient.status(400).send('Token invalid')
-    else next();
-}
-
-module.exports = verifyToken;
