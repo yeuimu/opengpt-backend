a2d30fb0393fff238b4bb045da7e4c6cf0df4220
Yoyoki <2197651308@qq.com>
2023-07-06 17:04:13 +0800

    The access rate of each user is limited by setting the number of requests it is allowed to send in a certain period of time.

--- middleware/index.js
+++ middleware/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+
 const middlewareCross = require('./cross');
+const middlewareRateLimit = requrie('express-rate-limit');
+
 const apiChat = require('./chat');
 const apiCode = require('./code');
 const apiRegister = require('./register');
@@ -7,6 +10,10 @@ const apiLogin = require('./login');
 
 module.exports = app => {
     app.use('*', middlewareCross);
+    app.use(middlewareRateLimit({
+        windowMs: 60 * 1000, // 一分钟
+        max: 100, // 最大请求数量
+    }));
     app.use(express.json());
     app.use('/chat', apiChat);
     app.use('/code', apiCode);
