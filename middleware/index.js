5216864ef9f608ddac96d8bd0e45ac68012410ad
Yoyoki <2197651308@qq.com>
2023-07-06 14:14:22 +0800

    Added to router register api

--- middleware/index.js
+++ middleware/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const middlewareCross = require('./cross');
 const apiChat = require('./chat');
 const apiCode = require('./code');
+const apiRegister = require('./register');
 
 module.exports = app => {
     app.use('*', middlewareCross);
     app.use(express.json());
     app.use('/chat', apiChat);
     app.use('/code', apiCode);
+    app.use('/register', apiRegister);
 }
